d3c8e5112bddeccd77b4e010712b9394b6e8fa0b
K022G1036@g.neec.ac.jp <K022G1036@g.neec.ac.jp>
2023-05-30 14:58:22 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,129 @@
+﻿#include <Novice.h>
+#include <cmath>
+
+const char kWindowTitle[] = "GC2C_08_タカハシ_シンイチ_MT3_00_01";
+
+struct Vector2 {
+	float x, y;
+};
+
+struct Vector3 {
+	float x, y, z;
+};
+
+struct Vector4 {
+	float x, y, z, w;
+};
+
+struct Matrix3x3 {
+	float m[3][3];
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2){
+	return { v1.x + v2.x,v1.y + v2.y,v1.z + v2.z };
+}
+
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	return { v1.x - v2.x,v1.y - v2.y,v1.z - v2.z };
+}
+
+// スカラー倍
+Vector3 Multiply(float scalar,const Vector3& v) {
+	return { scalar * v.x,scalar * v.y,scalar * v.z };
+}
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+
+// 長さ
+float Length(const Vector3& v) { return std::sqrt(Dot(v, v)); }
+
+// 正規化
+Vector3 Normalize(const Vector3&v)
+{
+	float length = Length(v);
+	return{ v.x / length,v.y / length,v.z / length };
+}
+
+Vector3 v1{ 1.0f,3.0f,-5.0f };
+Vector3 v2{ 4.0f,-1.0f,2.0f };
+float k = { 4.0f };
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		Vector3 resultAdd = Add(v1, v2);
+		Vector3 resultSubtract = Subtract(v1, v2);
+		Vector3 resultMultiply = Multiply(k, v1);
+		float resultDot = Dot(v1, v2);
+		float resultLength = Length(v1);
+		Vector3 resultNormalize = Normalize(v2);
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		VectorScreenPrintf(0, 0, resultAdd, "  : Add");
+		VectorScreenPrintf(0, kRowHeight, resultSubtract, "  : Subtract");
+		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Mulutiply");
+		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f  : Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f  : Length", resultLength);
+		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, "  :  Normalize");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
