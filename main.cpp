22c7cc6ebdcb6b543d1e93c4deee88d279141bbf
K022G1036@g.neec.ac.jp <K022G1036@g.neec.ac.jp>
2023-08-03 18:01:33 +0900

    ５完成

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2)
 {
 	Matrix4x4 result = m1;
 
-	return result = m2;
+	return result *= m2;
 }
 
 // 行列の積
@@ -84,25 +84,25 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 Matrix4x4 MakeRotateXMatrix(float radian)
 {
 	Matrix4x4 result{};
-	result.m[0][0] = 1;
-	result.m[0][1] = 0;
-	result.m[0][2] = 0;
-	result.m[0][3] = 0;
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
 
-	result.m[1][0] = 0;
+	result.m[1][0] = 0.0f;
 	result.m[1][1] = std::cos(radian);
 	result.m[1][2] = std::sin(radian);
-	result.m[1][3] = 0;
+	result.m[1][3] = 0.0f;
 
-	result.m[2][0] = 0;
+	result.m[2][0] = 0.0f;
 	result.m[2][1] -= std::sin(radian);
 	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0;
+	result.m[2][3] = 0.0f;
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 
 	return result;
 }
@@ -112,24 +112,24 @@ Matrix4x4 MakeRotateYMatrix(float radian)
 {
 	Matrix4x4 result{};
 	result.m[0][0] = std::cos(radian);
-	result.m[0][1] = 0;
+	result.m[0][1] = 0.0f;
 	result.m[0][2] -= std::sin(radian);
-	result.m[0][3] = 0;
+	result.m[0][3] = 0.0f;
 
-	result.m[1][0] = 0;
-	result.m[1][1] = 1;
-	result.m[1][2] = 0;
-	result.m[1][3] = 0;
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
 
 	result.m[2][0] = std::sin(radian);
-	result.m[2][1] = 0;
+	result.m[2][1] = 0.0f;
 	result.m[2][2] = std::cos(radian);
-	result.m[2][3] = 0;
+	result.m[2][3] = 0.0f;
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 
 	return result;
 }
@@ -140,50 +140,79 @@ Matrix4x4 MakeRotateZMatrix(float radian)
 	Matrix4x4 result{};
 	result.m[0][0] = std::cos(radian);
 	result.m[0][1] = std::sin(radian);
-	result.m[0][2] = 0;
-	result.m[0][3] = 0;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
 
 	result.m[1][0] -= std::sin(radian);
 	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = 0;
-	result.m[1][3] = 0;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
 
-	result.m[2][0] = 0;
-	result.m[2][1] = 0;
-	result.m[2][2] = 1;
-	result.m[2][3] = 0;
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 
 	return result;
 }
+
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
 
 // 拡大縮小行列
 Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 {
 	Matrix4x4 result{};
 	result.m[0][0] = scale.x;
-	result.m[0][1] = 0;
-	result.m[0][2] = 0;
-	result.m[0][3] = 0;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
 
-	result.m[1][0] = 0;
+	result.m[1][0] = 0.0f;
 	result.m[1][1] = scale.y;
-	result.m[1][2] = 0;
-	result.m[1][3] = 0;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
 
-	result.m[2][0] = 0;
-	result.m[2][1] = 0;
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
 	result.m[2][2] = scale.z;
-	result.m[2][3] = 0;
+	result.m[2][3] = 0.0f;
 
-	result.m[3][0] = 0;
-	result.m[3][1] = 0;
-	result.m[3][2] = 0;
-	result.m[3][3] = 1;
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 
 	return result;
 }
@@ -193,34 +222,35 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 Matrix4x4 MakeAffineMatrix(const Vector3& scale,const Vector3& rotate,const Vector3& translate)
 {
 	Matrix4x4 matScale = MakeScaleMatrix(scale);
+
 	Matrix4x4 matRotateX = MakeRotateXMatrix(rotate.x);
 	Matrix4x4 matRotateY = MakeRotateYMatrix(rotate.y);
 	Matrix4x4 matRotateZ = MakeRotateZMatrix(rotate.z);
-
 	Matrix4x4 matRot = matRotateX * matRotateY * matRotateZ;
-	Matrix4x4 matTranslate=
 
-	Matrix4x4 result = Multiply(Multiply(MakeRotateXMatrix(rotate.x), MakeRotateYMatrix(rotate.y)),MakeRotateZMatrix(rotate.z));
+	Matrix4x4 matTranslate = MakeTranslateMatrix(translate);
 
-	result.m[0][0] = scale.x;
-	result.m[0][1] = scale.x;
-	result.m[0][2] = scale.x;
-	result.m[0][3] = 0;
+	Matrix4x4 result =matScale*matRot*matTranslate;
 
-	result.m[1][0] = scale.y;
-	result.m[1][1] = scale.y;
-	result.m[1][2] = scale.y;
-	result.m[1][3] = 0;
+	//result.m[0][0] = scale.x;
+	//result.m[0][1] = scale.x;
+	//result.m[0][2] = scale.x;
+	//result.m[0][3] = 0;
 
-	result.m[2][0] = scale.z;
-	result.m[2][1] = scale.z;
-	result.m[2][2] = scale.z;
-	result.m[2][3] = 0;
+	//result.m[1][0] = scale.y;
+	//result.m[1][1] = scale.y;
+	//result.m[1][2] = scale.y;
+	//result.m[1][3] = 0;
 
-	result.m[3][0] = translate.x;
-	result.m[3][1] = translate.y;
-	result.m[3][2] = translate.z;
-	result.m[3][3] = 1;
+	//result.m[2][0] = scale.z;
+	//result.m[2][1] = scale.z;
+	//result.m[2][2] = scale.z;
+	//result.m[2][3] = 0;
+
+	//result.m[3][0] = translate.x;
+	//result.m[3][1] = translate.y;
+	//result.m[3][2] = translate.z;
+	//result.m[3][3] = 1;
 
 	return result;
 }
@@ -264,10 +294,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Vector3 scale{ 1.2f,0.79f,-2.1f };
 		Vector3 rotate{ 0.4f,1.43f,-0.8f };
 		Vector3 translate{ 2.7f,-4.15f,1.57f };
-		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		/*Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
 		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
 		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));*/
 
 		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
