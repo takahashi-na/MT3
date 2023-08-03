34d3668bbe66185c35a4616c60a0cb8de0926bf0
K022G1036@g.neec.ac.jp <K022G1036@g.neec.ac.jp>
2023-08-03 17:41:33 +0900

    a

--- main.cpp
+++ main.cpp
@@ -26,6 +26,29 @@ struct Matrix4x4 {
 	float m[4][4];
 };
 
+// 代入演算子
+Matrix4x4& operator*=(Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = {};
+
+	for (size_t i = 0; i < 4; i++) {
+		for (size_t j = 0; j < 4; j++) {
+			for (size_t k = 0; k < 4; k++) {
+				result.m[i][j] += m1.m[i][k]*m2.m[k][j];
+			}
+		}
+	}
+	m1 = result;
+	return m1;
+}
+
+// 二項演算子
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result = m1;
+
+	return result = m2;
+}
+
 // 行列の積
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 {
@@ -138,10 +161,45 @@ Matrix4x4 MakeRotateZMatrix(float radian)
 	return result;
 }
 
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+	Matrix4x4 result{};
+	result.m[0][0] = scale.x;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = scale.y;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = scale.z;
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
 
 // 3次元アフィン変換行列
 Matrix4x4 MakeAffineMatrix(const Vector3& scale,const Vector3& rotate,const Vector3& translate)
 {
+	Matrix4x4 matScale = MakeScaleMatrix(scale);
+	Matrix4x4 matRotateX = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 matRotateY = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 matRotateZ = MakeRotateZMatrix(rotate.z);
+
+	Matrix4x4 matRot = matRotateX * matRotateY * matRotateZ;
+	Matrix4x4 matTranslate=
+
 	Matrix4x4 result = Multiply(Multiply(MakeRotateXMatrix(rotate.x), MakeRotateYMatrix(rotate.y)),MakeRotateZMatrix(rotate.z));
 
 	result.m[0][0] = scale.x;
